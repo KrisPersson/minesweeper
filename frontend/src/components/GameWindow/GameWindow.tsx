fb2c2e8be59636462e849640870c5f9fa00780a3
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-08-28 21:00:29 +0200

    functionality for game endings

--- frontend/src/components/GameWindow/GameWindow.tsx
+++ frontend/src/components/GameWindow/GameWindow.tsx
@@ -1,19 +1,21 @@
 import "./GameWindow.css";
 import GameBoard from "../GameBoard/GameBoard";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { resetGame, setTimerIsRunning } from "../../store/gameSlice";
 import useTimer from "easytimer-react-hook";
 import { useEffect } from "react";
+import { RootState } from "../../store/store";
 
 export default function GameWindow() {
   const dispatch = useDispatch();
+  const [timer] = useTimer();
+
+  const size = 10;
 
   function handleRestart() {
     dispatch(resetGame());
     startTimer();
   }
-
-  const [timer] = useTimer();
 
   function startTimer() {
     timer.reset();
@@ -25,7 +27,7 @@ export default function GameWindow() {
   }
 
   function stopTimer() {
-    timer.stop();
+    timer.pause();
     dispatch(setTimerIsRunning(false));
   }
 
@@ -33,19 +35,36 @@ export default function GameWindow() {
     startTimer();
   }, []);
 
+  let amtMines = 0;
+  let flagsUsed = 0;
+  let revealed = 0;
+  const matrix = useSelector((state: RootState) => state.game.matrix);
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i].content === -1) amtMines += 1;
+    if (matrix[i].flagged) flagsUsed += 1;
+    if (matrix[i].revealed) revealed += 1;
+  }
+  const hasWon = size * size - revealed === amtMines;
+  if (hasWon) {
+    stopTimer();
+  }
+
   return (
     <article className="gamewindow wrapper_window">
       <header className="gamewindow__header">
-        <div className="stat_text">Player name: </div>
+        <div className="stat_text">
+          <img src="/flag.png" className="flag" /> {amtMines - flagsUsed}
+        </div>
         <div className="stat_text">
           Timer: <h2 className="timer">{timer.getTimeValues().toString()}</h2>
         </div>
       </header>
       <section className="game-window__main">
-        <GameBoard size={10} stopTimer={stopTimer} timer={timer} />
+        <GameBoard size={size} stopTimer={stopTimer} />
         <button onClick={handleRestart} className="restart-button">
           Restart
         </button>
+        {hasWon && <h2 className="you-won-text">YOU WON!</h2>}
       </section>
     </article>
   );
